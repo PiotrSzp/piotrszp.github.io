3668694287e6a87732b83f114e9e6ce86d0ec125
Piotr Szporko <piotr.szporko@gmail.com>
2019-08-30 18:04:53 +0200

    Initial commit

---
+++ precache-manifest.5f0c3e47d84afa8551fbf21878395741.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d549df3d455c99acd4d058aa4e334d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "965b2eee550c256d8901",
+    "url": "/static/css/main.4d4a6a9c.chunk.css"
+  },
+  {
+    "revision": "4026e62464be32a44743",
+    "url": "/static/js/2.cdf83212.chunk.js"
+  },
+  {
+    "revision": "965b2eee550c256d8901",
+    "url": "/static/js/main.18f139d3.chunk.js"
+  },
+  {
+    "revision": "bdc6f5f2e9f68f7c7c31",
+    "url": "/static/js/runtime~main.d5a5e262.js"
+  }
+]);
